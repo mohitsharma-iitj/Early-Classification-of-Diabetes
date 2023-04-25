5d9b62216fffb5052dcb8ddb63f271a91093ddab
mohitsharma-iitj <121308533+mohitsharma-iitj@users.noreply.github.com>
2023-04-26 03:45:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,32 +13,36 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    try:
+    # try:
         recieved = [int(x) for x in request.form.values()]
-        k = [np.array(recieved)]  #final_features
+        k = np.array(recieved)  #final_features
 
         codn = bool
         
-        if( k[0]>5 and k[0]<100)  :     codn = True                         #age
-        else                      :     codn = False
+        if( (k[0]>=16) and (k[0]<=90))  :     
+                codn = True                         #age
+        else:     
+                codn = False
                 
         for i in range(1,16):
-            if(k[i]==0 or k[i]==1):     codn = codn and True   
-            else                  :     codn = codn and False
+            if((k[i]==0) or (k[i]==1)):     
+                    codn = codn and True   
+            else:     
+                    codn = codn and False
 
         if(codn):
             model = pickle.load(open('model.pkl', 'rb'))
             transform_data_of_array = pickle.load(open('transform_data_of_array.pkl', 'rb'))
-            int_features = transform_data_of_array.transform(k)
-            prediction = model.predict(int_features)
+            int_features = transform_data_of_array.transform([k])
+            prediction = model.predict( int_features)
             # output = round(prediction[0], 2)
 
-            return render_template('index.html', prediction_text='predicted output is '.format(prediction))
+            return render_template('index.html', prediction_text='predicted output is $ {}'.format(prediction))
         else:
-            return render_template('index.html', prediction_text='Please give correct input, all either 0 or 1 (except age b/w 5:100)')
+            return render_template('index.html', prediction_text='Please give correct input, all either 0 or 1 (except age b/w 16:90) $ {} '.format(k))
         
-    except:
-        return render_template('index.html', prediction_text='Please give correct input, all either 0 or 1 (except age b/w 5:100)')
+    # except:
+        # return render_template('index.html', prediction_text='Please give correct input, all either 0 or 1 (except age b/w 5:100) $ {} '.format(codn))
 
 
 if __name__ == "__main__":
